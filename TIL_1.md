1bd3488a4f70a49fd8e6688148e4f5ae00353778
davidboybob <davidboy7780@gmail.com>
2022-03-14 17:28:02 +0900

    Initial commit

---
+++ TIL_1.md
@@ -0,0 +1,7 @@
+https://www.youtube.com/watch?v=IDVnZPjRCYg
+
+코틀린 3강으로 끝내기 유투브 강의로 안드로이드 프로젝트 하나 만들어보기
+
+
+
+# 1. 기본 문법편
---
+++ app/src/main/java/com/example/kotilinpractice/sample1.kt
@@ -0,0 +1,19 @@
+package com.example.kotilinpractice
+
+fun main() {
+    helloWorld()
+}
+
+
+//1. 함수
+// fun name(parameter) return {}
+fun helloWorld() : Unit {
+    println("Hello, World!")
+}
+//JAVA
+
+
+// return 없으면,
+fun add(a: Int, b: Int) : Int {
+    return a+b
+}
